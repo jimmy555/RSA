66f553225f01befa01df7d2daaf05c7cd3319c6d
Jimmy van der Schalk <jimmy555@live.nl>
2018-04-17 15:24:05 +0200

    Added comments and changed some code for efficiency

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,16 @@ public class Main {
             ENCRYPTION
          */
         // Make sure the arguments are not empty and that there are two arguments, to avoid getting a crash
-        /*if (args != null && args.length == 2){
+        if (args != null && args.length == 2){
 
             // Get p,q and the message from the arguments
             String nCombined = args[0];
-            int p = Integer.valueOf(nCombined.split("\\*")[0]);
-            int q = Integer.valueOf(nCombined.split("\\*")[1]);
+            int n = Integer.valueOf(nCombined);
             String message = args[1];
 
             // Start time for timing how long the encryption takes
             long startTime = System.currentTimeMillis();
 
-            int n = p * q;
             // Instantiate the RSA class
             RSA rsa = new RSA(n);
 
@@ -37,7 +35,7 @@ public class Main {
 
         } else {
             System.out.println("Make sure there are exactly 2 arguments");
-        }*/
+        }
 
         /*
             DECRYPTION
@@ -45,13 +43,14 @@ public class Main {
          */
         // Make sure the arguments are not empty and that there are two arguments, to avoid getting a crash
 
-        if (args != null && args.length == 3){
+        /*if (args != null && args.length == 3){
 
             // Get p and the message from the arguments
             int n = Integer.valueOf(args[0]);
             int e = Integer.valueOf(args[1]);
             String message = args[2];
             String [] letters = message.split(",");
+
             ArrayList<Integer> integers = new ArrayList<>();
             for (String letter : letters) {
                 int letterCode = Integer.valueOf(letter.replace(" ", ""));
@@ -70,15 +69,15 @@ public class Main {
             long finishTime = System.currentTimeMillis();
 
             // Print output
+            System.out.println("Output: e is: " + rsa.getE());
             System.out.println("Output: p is: " + rsa.getP());
             System.out.println("Output: q is: " + rsa.getQ());
-            System.out.println("Output: e is: " + rsa.getE());
+            System.out.println("Output: d is: " + rsa.getD());
             System.out.println("Output: Message after decoding is: " + decryptedMessage);
             System.out.println("Output: Amount of time busy encoding was: " + (finishTime - startTime) + " ms");
 
         } else {
-            System.out.println("Make sure there are exactly 2 arguments");
-        }
-
+            System.out.println("Make sure there are exactly 3 arguments");
+        }*/
     }
 }
--- src/RSA.java
+++ src/RSA.java
@@ -15,8 +15,19 @@ public class RSA {
 	// Modular multiplicative inverse of e
 	private int d;
 
-
 	// For Encryption
+	public RSA(int n){
+		this.n = n;
+		factorize(n);
+
+		// Least common multiple
+		int lcm = lcm(this.p, this.q);
+
+		// public exponent e
+		this.e = calculateE(lcm);
+	}
+
+	// For Decryption
 	public RSA(int n, int e){
 		this.n = n;
 		factorize(n);
@@ -24,18 +35,10 @@ public class RSA {
 		// Least common multiple
 		int lcm = lcm(this.p, this.q);
 		this.e = e;
+
+		// private exponent d
 		d = calculateD(e, lcm);
 
-	}
-
-	public RSA(int n){
-		this.n = n;
-		factorize(n);
-
-		// Least common multiple
-		int lcm = lcm(this.p, this.q);
-		this.e = calculateE(lcm);
-		this.d = calculateD(this.e, lcm);
 	}
 
 	public int getP(){
@@ -50,40 +53,51 @@ public class RSA {
 		return this.e;
 	}
 
+	public int getD(){
+		return this.d;
+	}
+
+	// Encrypt function
 	public ArrayList<Integer> encrypt(String message) {
 		ArrayList<Integer> encryptedMessage = new ArrayList<>();
 
+		// Loop through all the characters in the message
 		for(int i = 0; i < message.length(); i ++){
+			// Get the ASCII value of the character
 			int code = Character.codePointAt(message, i);
 			// Encrypt the character
 			BigInteger encryptedChar = valueOf(code).pow(this.e).remainder(valueOf(this.n));
-			// Convert BigInteger to int
-			char c = (char)encryptedChar.intValue();
-			encryptedMessage.add((int)c);
+			// Convert BigInteger to char
+			int c = encryptedChar.intValue();
+			// Add int to the encrypted message
+			encryptedMessage.add(c);
 		}
 
 		return encryptedMessage;
 	}
 
+	// Decrypt function
 	public String decrypt(ArrayList<Integer> encryptedMessage){
 		String message = "";
 
+		// Loop through the encrypted string
 		for(int i = 0; i < encryptedMessage.size(); i++) {
 			int code = encryptedMessage.get(i);
 			// Decrypt the character
 			BigInteger decryptedChar = valueOf(code).pow(this.d).remainder((valueOf(this.n)));
-			// Convert BigInteger to String
+			// Convert BigInteger to char
 			char c = (char)decryptedChar.intValue();
+			// Add character to the message
 			message += String.valueOf(c);
 		}
 
 		return message;
 	}
 
-	private int calculateD(int e, int phi) {
-		e = e % phi;
-		for (int i = 1; i < phi; i++) {
-			int a = (i * e) % phi;
+	// Calculate the private exponent, or also known as D
+	private int calculateD(int e, int lcm) {
+		for (int i = 1; i < lcm; i++) {
+			int a = (i * e) % lcm;
 			if (a == 1) {
 				return i;
 			}
@@ -92,10 +106,11 @@ public class RSA {
 		return 0;
 	}
 
+	// Calculate the public exponent, or also known as E
 	private static int calculateE(int lcm) {
 		int e = 2;
 		while(1 < e && e < lcm){
-			if (isPrime(e) && relativelyPrime(e, lcm)) {
+			if (relativelyPrime(e, lcm)) {
 				return e;
 			}
 			e++;
@@ -104,10 +119,12 @@ public class RSA {
 		return 0;
 	}
 
+	// Calculate the lcm with Reduction by the Greatest Common Divisor
 	private int lcm(int p, int q) {
 		return ((p-1)*(q-1))/gcd(p-1, q-1);
 	}
 
+	// Factorize p and q from N
 	public void factorize(int n){
 		List<Integer> factorizedN = primeFactors(n);
 		p = factorizedN.get(0);
@@ -131,6 +148,7 @@ public class RSA {
 		return factors;
 	}
 
+	// Get the greates common divisor of two variables
 	private static int gcd(int a, int b) {
 		int t;
 		while(b != 0){
@@ -141,7 +159,9 @@ public class RSA {
 		return a;
 	}
 
+	// Check if e and lcm are relatively prime
 	private static boolean relativelyPrime(int e, int lcm) {
+		// If greates comman divisor of e and lcm is equal to one, they are relatively prime
 		return gcd(e,lcm) == 1;
 	}
 
